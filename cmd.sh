26bad4cc66459fbac429e0fdcbaaebb2984d6a9e
Mike Farrow <contact@mikefarrow.co.uk>
2017-09-06 23:36:54 +0100

    fixing shellcheck errors

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if docker service ps $WERCKER_DOCKER_SWARM_SERVICE ; then
-    docker service update --detach=false --update-order=start-first --update-failure-action=rollback --rollback-order=start-first --with-registry-auth --image $WERCKER_DOCKER_SWARM_IMAGE:$WERCKER_DOCKER_SWARM_TAG $WERCKER_DOCKER_SWARM_SERVICE
+if docker service ps "$WERCKER_DOCKER_SWARM_SERVICE" ; then
+    docker service update --detach=false --update-order=start-first --update-failure-action=rollback --rollback-order=start-first --with-registry-auth --image "$WERCKER_DOCKER_SWARM_IMAGE:$WERCKER_DOCKER_SWARM_TAG" "$WERCKER_DOCKER_SWARM_SERVICE"
 else
     echo please deploy service/stack:
     echo "$WERCKER_DOCKER_SWARM_IMAGE:$WERCKER_DOCKER_SWARM_TAG"
